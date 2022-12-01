3b9692a1156b8be6bac5ca9e6c5bfd3526d80828
D3b0rt <juanldbortoli@gmail.com>
2022-12-01 19:48:43 -0300

    creacion
    agrega los archivos necesarios

---
+++ test.js
@@ -0,0 +1,9 @@
+document.querySelector('#siguiente-paso').onclick = function(event){
+    const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
+    const cantidadIntegrantes= Number($cantidadIntegrantes.value);
+
+    borrarAnteriores();
+    crearIntegrantes();
+
+    event.preventDefault();
+    }
